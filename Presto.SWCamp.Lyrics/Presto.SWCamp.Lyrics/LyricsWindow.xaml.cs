0c8bf3721a825f752e42d146950a7955f688fdc9
Kang <sfui@naver.com>
2018-11-24 16:09:33 +0900

    Update LyricsWindow.xaml.cs

--- Presto.SWCamp.Lyrics/Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
+++ Presto.SWCamp.Lyrics/Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
@@ -15,15 +15,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
-/*
- * String[] lines = File.ReadAllLines("@"");
- * foreach(var line in lines)
- * {
- * var datas = line.split(']');
- * messagebox.show(line);
- * }
- */
-
 namespace Presto.SWCamp.Lyrics
 {
     /// <summary>
@@ -31,7 +22,7 @@ namespace Presto.SWCamp.Lyrics
     /// </summary>
     public partial class LyricsWindow : Window
     {
-
+        SortedList<TimeSpan, string> _lyrics = new SortedList<TimeSpan, string>();
 
         public LyricsWindow()
         {
@@ -41,32 +32,44 @@ namespace Presto.SWCamp.Lyrics
 
         private void Player_StreamChanged(object sender, Common.StreamChangedEventArgs e)
         {
+            SortedList<TimeSpan, string> _lyrics = new SortedList<TimeSpan, string>();
             textLyrics.Text = null;
             var fileName = PrestoSDK.PrestoService.Player.CurrentMusic.Path;
             var lrcName = Path.GetFileNameWithoutExtension(fileName) + ".lrc";
             var path = Path.Combine(Path.GetDirectoryName(fileName), lrcName);
-            //MessageBox.Show(path.ToString());
             var lines = File.ReadAllLines(path);
+
             for (int i = 3; i < lines.Length; i++)
             {
                 var splitData = lines[i].Split(']');
                 var time = TimeSpan.ParseExact(splitData[0].Substring(1).Trim(), @"mm\:ss\.ff", CultureInfo.InvariantCulture);
-                var timer = new DispatcherTimer
-                {
-                    Interval = TimeSpan.FromMilliseconds(100)
-                };
-                timer.Tick += Timer_Tick;
-                //textLyrics.Text = time.ToString() + splitData[1] + '\n';
-                timer.Start();
-                
+              
+                _lyrics.Add(time, splitData[1]);
+
                 //textLyrics.Text = time.ToString();
-                //MessageBox.Show(time.TotalMilliseconds.ToString());
+                //MessageBox.Show(_lyrics.Keys[i-3].TotalMilliseconds.ToString());
             }
+
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            textLyrics.Text = PrestoSDK.PrestoService.Player.Position.ToString();
+            for (int i = 0; i < _lyrics.Count; i++)
+            {
+                if (_lyrics.Keys[i].TotalMilliseconds >= PrestoSDK.PrestoService.Player.Position && _lyrics.Keys[Math.Min(_lyrics.Count-1,i+1)].TotalMilliseconds <= PrestoSDK.PrestoService.Player.Position)
+                {
+                    textLyrics.Text = _lyrics.Values[i];
+                }
+            }
+
+            // times[i] <> PrestoSDK.PrestoService.Player.Position
+
         }
     }
 }
